265683b6f1d7848f4e38b709e5f15067d858507b
Sergey <hlypik0@gmail.com>
2021-06-07 14:52:25 -0400

    done program

--- main.cpp
+++ main.cpp
@@ -5,5 +5,20 @@ using namespace std;
 int main () 
 {
   int number = 0;
-  cout << "Enter number: "; 
+  cout << "Enter number: "; cin >> number;
+  int power = 0;
+  cout << "Enter power: "; cin >> power;
+  int result = number;
+  if (power == 0) 
+  {
+    cout << "number = 1" << endl;
+  }
+  else 
+  {
+    for (int i = 0; i < power - 1; i++) 
+    {
+      result = result * number;
+    }
+    cout << "number = " << result << endl;  
+  }
 }
